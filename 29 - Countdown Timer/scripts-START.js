b3e2d26f87020c3c4d9b6b04a28e1a3468e0d5a2
Rey Mercado <reymercado.usa@gmail.com>
2020-01-09 14:07:50 -0800

    set up condition to stop when seconds reach 0, using clearInterval(), and by setting setInterval to either a global variable or an IIFE variable

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,16 +1,26 @@
+
+
 /* function timer(seconds) {
   setInterval(function() {
     seconds--;
   }, 1000);
 }
 // above code is faulty because for some reason setInterval will stop running when it supposed to never stop */
+
+let countdown; // we have placed it in the global name space, but alternatively we could have placed this variable instead in an iife
 function timer(seconds) {
   //to figure out when timer started, write below
   const now = Date.now();  //old equivalent way was (new Date()).getTime();
   const then = now + seconds * 1000; //multiplied by 1000 because now is in milliseconds
   // console.log({now, then});
-  setInterval(() => {
+  countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000); //converting to seconds from milliseconds
+    // check if we should stop it! - in order to stop it we need to store countdown in a separate global variable
+    if(secondsLeft < 0) { // correction - to display correctly it is < 0 not <= 0
+      clearInterval(countdown);
+      return;
+    }
+    // display it
     console.log(secondsLeft);
   }, 1000)
 }
