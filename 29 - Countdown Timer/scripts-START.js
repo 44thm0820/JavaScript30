90c28a02f04450b36c3a5d160191819837a8faf4
Rey Mercado <reymercado.usa@gmail.com>
2020-01-09 13:53:21 -0800

    using Math.round to count down without decimals

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -8,7 +8,11 @@ function timer(seconds) {
   //to figure out when timer started, write below
   const now = Date.now();  //old equivalent way was (new Date()).getTime();
   const then = now + seconds * 1000; //multiplied by 1000 because now is in milliseconds
-  console.log({now, then});
+  // console.log({now, then});
+  setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000); //converting to seconds from milliseconds
+    console.log(secondsLeft);
+  }, 1000)
 }
 
 //next thing we have to do is to display every second the amount of time left
