4fc04f2097f63a6e51f6a926de7b3d56584d4c68
Rey Mercado <reymercado.usa@gmail.com>
2020-01-09 11:50:01 -0800

    confirming that function timer works

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,6 +1,15 @@
-function timer(seconds) {
+/* function timer(seconds) {
   setInterval(function() {
     seconds--;
   }, 1000);
 }
-// above code is faulty because for some reason setInterval will stop running when it supposed to never stop
+// above code is faulty because for some reason setInterval will stop running when it supposed to never stop */
+function timer(seconds) {
+  //to figure out when timer started, write below
+  const now = Date.now();  //old equivalent way was (new Date()).getTime();
+  const then = now + seconds * 1000; //multiplied by 1000 because now is in milliseconds
+  console.log({now, then});
+}
+
+//next thing we have to do is to display every second the amount of time left
+
